c207a4814b4bf8ab7fb72e4c75a6f3785390beb9
fransurf <flcknight@gmail.com>
2022-06-10 16:32:01 +0100

    basic index plus fun with glitch

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,62 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const App = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/boards/') // * <-- replace with your endpoint
-      console.log(data)
-    }
-    getData()
-  })
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
-  return <h1>Welcome to Women that Shred</h1>
+const App = () => {
+
+  const [boards, setBoards] = useState([])
+  const [errors, setErrors] = useState(false)
+
+  useEffect(() => {
+    const getBoards = async () => {
+      try {
+        const { data } = await axios.get('/api/boards/') 
+        console.log('🏆 Got the boards data!', data)
+
+        setBoards(data)
+
+      } catch (error) {
+        console.log('🥺 error getting your board data 🥺', error)
+        setErrors(true)
+      }
+    }
+
+    getBoards()
+  }, [])
+
+
+  return (
+    <Container className='boards-page-container'>
+      <h1 className='glitch' data-text="Welcome to Women that Shred">🏆 Welcome to Women that Shred 🏆</h1>
+      <h1 className='glow'>🏆 Welcome to Women that Shred 🏆</h1>
+      <div className='scanlines'></div>
+      <Row className='justify-contents-center'>
+        {boards.map(board => {
+          const { id, make, model, price, description, link } = board
+          return (
+            <Col key={id} md='6' lg='4' className='board'>
+              {/* Put a link to singular board here? */}
+              <Card>
+                <Card.Img className='board-img' variant="left" src="URL" />
+                <Card.Body>
+                  <Card.Title>{make} - {model}</Card.Title>
+                  {/* <Card.Title className='glow'>{make} - {model}</Card.Title>
+                  <div className='scanlines'></div> */}
+                  <Card.Text>£{price}</Card.Text>
+                  <Card.Text>{description}</Card.Text>
+                  <Card.Text>{link}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+    </Container>
+  )
 }
 
 export default App
