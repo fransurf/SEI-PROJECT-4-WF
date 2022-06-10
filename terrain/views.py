1cfc781183772e8bd771acb6175e0c4e19a2ab03
fransurf <flcknight@gmail.com>
2022-06-10 13:12:23 +0100

    BACKEND COMPLETE

--- terrain/views.py
+++ terrain/views.py
@@ -3,11 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Terrain
-from .serializers.populated import PopulatedTerrainSerializer
+from .serializers.common import TerrainSerializer
 
 class TerrainListView(APIView):
 
   def get(self, _request):
     terrain = Terrain.objects.all()
-    serialized_terrains = PopulatedTerrainSerializer(terrain, many=True)
+    serialized_terrains = TerrainSerializer(terrain, many=True)
     return Response(serialized_terrains.data, status.HTTP_200_OK)
