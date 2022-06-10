319816ba0b728f4f8581f119427e23967fa765a2
fransurf <flcknight@gmail.com>
2022-06-10 12:25:47 +0100

    backend complete. save before remigrate

--- pre_loved/models.py
+++ pre_loved/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Pre_loved(models.Model):
@@ -6,6 +7,8 @@ class Pre_loved(models.Model):
   model = models.CharField(max_length=100, default=None)
   board_img = models.CharField(max_length=200, default=None)
   price = models.PositiveIntegerField(default=None)
+  description = models.TextField(max_length=500, default=None)
+  created_at = models.DateTimeField(auto_now_add=True) # timestamp = time of submission
   owner = models.ForeignKey(
     'jwt_auth.User', # the model to look for the FK on
     related_name = 'pre_loved', # the field name to look for
--- pre_loved/views.py
+++ pre_loved/views.py
@@ -14,7 +14,7 @@ from .serializers.common import Pre_LovedSerializer
 # GET all preloved & POST a preloved
 
 class PrelovedListView(APIView):
-  # permission_classes = (IsAuthenticated, )
+  permission_classes = (IsAuthenticated, )
 
   def get(self, _request):
     preloved = Pre_loved.objects.all()
@@ -25,6 +25,8 @@ class PrelovedListView(APIView):
 # ? Owner is not updating - always inputs as 1 (default)
   def post(self, request):
     deserialized_preloved = Pre_LovedSerializer(data=request.data)
+    request.data['owner'] = request.user.id
+    
     print('preloved post data ->', request.data)
     try:
       deserialized_preloved.is_valid()
@@ -53,13 +55,10 @@ class PrelovedDetailView(APIView):
 
 
 
-  # def delete(self, _request, pk):
-  #   try: 
-  #     preloved_to_delete = Pre_loved.objects.get(pk=pk)
-  #     print(preloved_to_delete.data)
-  #     preloved_to_delete.delete()
-  #     return Response(status=status.HTTP_204_NO_CONTENT)
-  #   except Pre_loved.DoesNotExist as e:
-  #     print(e)
-  #     raise NotFound({ "detail": str(e) })
+  def delete(self, _request, pk):
+    preloved_to_delete = self.get_preloved(pk)
+    print("deleting a board ->", preloved_to_delete)
+    preloved_to_delete.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 
