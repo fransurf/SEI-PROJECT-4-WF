f987f251137572e62b78691507596aee7c378cdc
fransurf <flcknight@gmail.com>
2022-06-13 12:30:34 +0100

    boards on selection page mocked

--- client/src/components/landingPage/landingPageSelection.js
+++ client/src/components/landingPage/landingPageSelection.js
@@ -5,27 +5,31 @@ import Container from 'react-bootstrap/Container'
 
 const LandingPageSelection = () => {
   return (
-    <Container className='selection-page-container'>
+    <Container className='landing-page-container'>
       <section className='boards-selection'>
-        <div id='piste'>
-          <img src='piste-board-url' alt='piste bashers selection' />
-          <h2>Pister bashers</h2>
+        <div className='board-selector' id='piste'>
+          {/* <img src='piste-board-url' alt='piste bashers selection' /> */}
+          <h2 className='selector-text' >Pister bashers {'   >>>'}</h2>
         </div>
-        <div id='powder'>
-          <img src='powder-board-url' alt='powder days selection' />
-          <h2>Powder days</h2>
+        <div className='board-selector' id='powder'>
+          {/* <img src='powder-board-url' alt='powder days selection' /> */}
+          <h2 className='selector-text'>Powder days {'   >>>'}</h2>
         </div>
-        <div id='park'>
-          <img src='park-board-url' alt='park selection' />
-          <h2>Park</h2>
+        <div className='board-selector' id='park'>
+          {/* <img src='park-board-url' alt='park selection' /> */}
+          <h2 className='selector-text'>Park {'   >>>'}</h2> 
         </div>
-        <div id='all-mountain'>
-          <img src='all-mountain-board-url' alt='all mountain selection' />
-          <h2>All mountain</h2>
+        <div className='board-selector' id='all-mountain'>
+          {/* <img src='all-mountain-board-url' alt='all mountain selection' /> */}
+          <h2 className='selector-text'>All mountain {'   >>>'}</h2>
         </div>
-        <div id='all'>
-          <img src='all-board-url' alt='all boards selection' />
-          <h2>Cant decide - show me them all</h2>
+        <div className='board-selector' id='beginner'>
+          {/* <img src='beginner' alt='beginner selection' /> */}
+          <h2 className='selector-text'>Its not all about the pros {'   >>>'}</h2>
+        </div>
+        <div className='board-selector' id='all'>
+          {/* <img src='all-board-url' alt='all boards selection' /> */}
+          <h2 className='selector-text'>Show me them all {'   >>>'}</h2>
         </div>
       </section>
       <img className='landing-img' src='../../assets/general/bwPhoto.jpeg' alt='female boarder photo' />
